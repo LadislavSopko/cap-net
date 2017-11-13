3961ac2bc195d084266c1ba229ef854e00e42681
Damian Hintz <damian.hintz@schange.com>
2017-11-13 15:41:48 +0100

    UPDATED: release notes

--- src/CAPNet/Properties/AssemblyInfo.cs
+++ src/CAPNet/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Reflection;
 [assembly: AssemblyTitle("cap-net")]
 [assembly: AssemblyDescription("A Common Alerting Protocol serialization library for the .NET framework.")]
 [assembly: AssemblyProduct("cap-net")]
-[assembly: AssemblyCopyright("Copyright © 2015 Matt Chandler and contributors")]
+[assembly: AssemblyCopyright("Copyright © 2017 Matt Chandler and contributors")]
 
 [assembly: AssemblyCompany("Teamnet")]
-[assembly: AssemblyVersion("3.0.0.0")]
-[assembly: AssemblyFileVersion("3.0.0.0")]
+[assembly: AssemblyVersion("3.0.1.0")]
+[assembly: AssemblyFileVersion("3.0.1.0")]
 [assembly: AssemblyInformationalVersion("3.0")]
 
 [assembly: CLSCompliant(true)]
